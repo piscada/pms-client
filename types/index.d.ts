c43b1dcff6b26508a8e36469d4bed4092bbf184d
magnuguli <magnus.gule@piscada.com>
2022-02-11 11:33:10 +0100

    [1p84jdg] Upgraded v and types

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+import MediaServerClient from "./lib/MediaServerClient.js";
+import PeerConnectionClient from "./lib/PeerConnectionClient.js";
+import MedoozeConnector from "./lib/MedoozeConnector.js";
+import MedoozePlayer from "./lib/MedoozePlayer.js";
+export { MediaServerClient, PeerConnectionClient, MedoozeConnector, MedoozePlayer };
+//# sourceMappingURL=index.d.ts.map
