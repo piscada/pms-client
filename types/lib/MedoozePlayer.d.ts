c541b0c2828f06133b3b3612869a2f09cf61dee0
magnuguli <magnus.gule@piscada.com>
2023-06-08 14:48:03 +0200

    [861mwwnmw] Updated mapping

--- types/lib/MedoozePlayer.d.ts
+++ types/lib/MedoozePlayer.d.ts
@@ -13,7 +13,7 @@ export default class MedoozePlayer {
     client: MediaServerClient;
     reconnect: () => void;
     streamPromise: Promise<any>;
-    createPeerConnection(cli: any, resolve: any): Promise<any>;
+    createPeerConnection(cli: any, resolve: any, camId: any): Promise<any>;
     stream: MediaStream;
     reConnectListener(): void;
 }
