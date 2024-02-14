a717c5c90fb3db77ce5349bcf506f46431744fee
magnuguli <magnus.gule@piscada.com>
2024-02-14 10:00:25 +0100

    [8693u2kpm] Fixed undef refs. Build on test. Not complete.

--- tests/basic.js
+++ tests/basic.js
@@ -1,31 +1,25 @@
-import { MedoozeConnector as medoozeConnect } from '../lib/MedoozeConnector.js'
-import { MedoozePlayer } from '../lib/MedoozePlayer.js'
-
-
+import { MedoozeConnector, MedoozePlayer } from '../types/index.js'
 
 const connectionConfig = {
-  host: "pms",
+  host: '172.25.25.151',
   httpOnly: true,
-  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MDAzNDczMTR9.WsryoZ6JGRHXeBopzH-SMcZUXhMvjdYc5YDwFci5F0Y",
+  token:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MDAzNDczMTR9.WsryoZ6JGRHXeBopzH-SMcZUXhMvjdYc5YDwFci5F0Y',
   port: 8000,
   isCloudServer: false
 }
 
-const pmsConnection = await medoozeConnect(connectionConfig)
+const pmsConnection = await MedoozeConnector(connectionConfig)
 pmsConnection.cameras = pmsConnection.cameraList
 
-// Get server-settings:
-const pms = store.state.MultiCamera.pms.servers.find(
-  (el) => el.host === this.stream.pmsHostname
-)
-
 const conf = {
-  id: camera.id,
-  pms,
-  onReconnect: this.onReconnect
+  id: 'PI-02',
+  pmsConnection
 }
 
-this.playerInstance = new MedoozePlayer(conf)
+const playerInstance = new MedoozePlayer(conf)
 
 // Must await the MediaStream since it is event-based
-this.srcObject = await this.playerInstance.streamPromise
+const srcObject = await playerInstance.streamPromise
+
+console.log({ srcObject })
