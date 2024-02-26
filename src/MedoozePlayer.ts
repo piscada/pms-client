b2d0c79a3c03b0e96e593bf9b940c2b0582cce6d
magnuguli <magnus.gule@piscada.com>
2024-02-26 15:43:19 +0100

    [8693u2kpm] Tested refactor

--- src/MedoozePlayer.ts
+++ src/MedoozePlayer.ts
@@ -1,9 +1,9 @@
 import { info, success } from '@piscada/snackbar'
 import MediaServerClient from './MediaServerClient'
-import yaps from './yaps'
 import TransactionManager from 'transaction-manager'
 import PeerConnectionClient from './PeerConnectionClient'
 import { PMSConnector } from './MedoozeConnector'
+import yaps from './yaps'
 
 interface PlayerConfig {
   id: string
@@ -24,114 +24,117 @@ interface ViewResponse extends TransactionManagerError {
   viewerId?: string
 }
 
-export default class MedoozePlayer {
+class MedoozePlayer {
   id: string
-  onReconnect?: (instance: MedoozePlayer) => void
   instanceID: string
   panelNumber: number
-  pcc: PeerConnectionClient
+  onReconnect?: (instance: MedoozePlayer) => void
   ws: WebSocket
   tm: TransactionManager
-  viewerId: string | null
   client: MediaServerClient
+  pcc: PeerConnectionClient
+  viewerId: string | null
   streamPromise: Promise<MediaStream | null>
   stream: MediaStream | null
-  reconnect: () => void
 
   constructor(config: PlayerConfig) {
-    // Constructor
     this.createPlayer(config)
   }
 
-  createPlayer(config: PlayerConfig) {
-    const { id, pms, instanceID, panelNumber = 1, onReconnect } = config
+  createPlayer(config: PlayerConfig): void {
+    this.id = config.id
+    this.instanceID = config.instanceID
+    this.panelNumber = config.panelNumber || 1
+    this.onReconnect = config.onReconnect
+    this.ws = config.pms.ws
+    this.tm = config.pms.tm
 
-    this.id = id
-    this.onReconnect = onReconnect
-    this.instanceID = instanceID
-    this.panelNumber = panelNumber
-
-    this.pcc = null
-    this.ws = pms.ws
-    this.tm = pms.tm
-    this.viewerId = null
-
-    this.stop = this.stop.bind(this)
-    this.pause = this.pause.bind(this)
-    this.unPause = this.unPause.bind(this)
-
-    // Create managed peer connection
     this.client = new MediaServerClient(this.tm)
-
-    // Auto reconnect listener if onReconnect is enabled
-    if (onReconnect) {
-      this.reconnect = () => {
-        if (this.pcc && this.client) this.reConnectListener()
-      }
-      this.ws.addEventListener('close', this.reconnect, { once: true })
+    this.pcc = null
+    this.viewerId = null
+    this.stream = null
+    this.streamPromise = this.createStreamPromise()
+    if (this.onReconnect) {
+      this.setupReconnectListener()
     }
-
-    // Important one here:
-    // Need to resolve this.streamPromise to get srcObject
-
-    this.streamPromise = new Promise((resolve, reject) => {
-      ;(async () => {
-        try {
-          const pcc = await this.createPeerConnection(
-            this.client,
-            resolve,
-            this.id
-          )
-          this.pcc = pcc
-
-          const res: ViewResponse = await this.tm.cmd('view', {
-            id: this.id,
-            instance: this.instanceID,
-            pcId: pcc.id
-          })
-          console.log({ response: res })
-
-          if (res.error) {
-            return reject(res.error)
-          }
-
-          this.viewerId = res.viewerId
-        } catch (err) {
-          return reject(err)
-        }
-      })()
-    })
   }
 
-  async createPeerConnection(
-    cli: MediaServerClient,
-    resolve: (stream: MediaStream) => void,
-    camId: string
-  ) {
-    const pcc: PeerConnectionClient = await cli.createManagedPeerConnection()
+  private async createStreamPromise(): Promise<MediaStream | null> {
+    try {
+      const pcc = await this.createPeerConnection(this.client, this.id)
+      this.pcc = pcc
+      const res: ViewResponse = await this.tm.cmd('view', {
+        id: this.id,
+        instance: this.instanceID,
+        pcId: pcc.id
+      })
+      if (res.error) {
+        throw new Error(res.error)
+      }
+      this.viewerId = res.viewerId
+      return this.stream
+    } catch (error) {
+      console.error('Error creating stream:', error)
+      return null
+    }
+  }
 
-    // On new remote tracks
+  private async createPeerConnection(
+    cli: MediaServerClient,
+    camId: string
+  ): Promise<PeerConnectionClient> {
+    const pcc = await cli.createManagedPeerConnection()
     pcc.ontrack = (event) => {
       if (event.remoteTrackId === camId) {
         const track = event.track
         this.stream = new MediaStream([track])
-        resolve(this.stream)
       }
     }
-
     return pcc
   }
 
-  pause() {
-    const id = this.id
+  private setupReconnectListener(): void {
+    const delay = this.panelNumber * 1000
+    const id = yaps.subscribe('pms_reconnect', (freshPMS: PMSConnector) => {
+      success(`New WS found. Connecting ${this.id} in ${delay}ms`, 'success')
+      setTimeout(() => {
+        yaps.unsubscribe(id)
+        this.reconnect(freshPMS)
+      }, delay)
+    })
+    info('added subscribe in player reconnectListener', id)
+  }
 
+  private reconnect(freshPMS: PMSConnector): void {
+    this.stop()
+    this.createPlayer({
+      id: this.id,
+      pms: freshPMS,
+      instanceID: this.instanceID,
+      panelNumber: this.panelNumber,
+      onReconnect: this.onReconnect
+    })
+    this.onReconnect?.(this)
+  }
+
+  private stop(): void {
+    this.onReconnect && this.ws.removeEventListener('close', this.reconnect)
     if (this.id) {
-      console.log('unviewing', id)
-      this.tm.cmd('unview', { id, instance: this.instanceID })
+      console.log('unviewing', this.id)
+      this.tm.cmd('unview', { id: this.id, instance: this.instanceID })
+    }
+    this.client.ns !== null && this.client.stop()
+    this.pcc && this.pcc.close()
+  }
+
+  pause(): void {
+    if (this.id) {
+      console.log('unviewing', this.id)
+      this.tm.cmd('unview', { id: this.id, instance: this.instanceID })
     }
   }
 
-  async unPause() {
+  async unPause(): Promise<string | Error | void> {
     if (this.pcc && this.id) {
       try {
         const res: ViewResponse = await this.tm.cmd('view', {
@@ -140,11 +143,9 @@ export default class MedoozePlayer {
           pcId: this.pcc.id
         })
         console.log({ response: res })
-
         if (res.error) {
           return res.error
         }
-
         this.viewerId = res.viewerId
       } catch (err) {
         return err
@@ -152,53 +153,7 @@ export default class MedoozePlayer {
     }
   }
 
-  stop() {
-    const id = this.id
-
-    // Remove the single listener if defined
-    this.onReconnect && this.ws.removeEventListener('close', this.reconnect)
-
-    // Only if WS is open. TM gives error
-    if (id) {
-      console.log('unviewing', id)
-      this.tm.cmd('unview', { id, instance: this.instanceID })
-    }
-
-    // Clean up event listener and namespace + tm
-    this.client.ns !== null && this.client.stop()
-
-    // Closing the PeerConnection directly. No need to wait for ontrackended.
-    this.pcc && this.pcc.close() // WORKS
-  }
-
-  reConnectListener() {
-    const delay = this.panelNumber * 1000
-
-    // Subscribe to when pms reconnects
-    const id = yaps.subscribe('pms_reconnect', (freshPMS: PMSConnector) => {
-      success(`New WS found. Connecting ${this.id} in ${delay}ms`, 'success')
-
-      setTimeout(() => {
-        // Remove the subscription
-        yaps.unsubscribe(id)
-
-        // Clean up pcc and client
-        this.stop()
-
-        // Recreate the player, and thus "this"
-        this.createPlayer({
-          id: this.id,
-          pms: freshPMS,
-          instanceID: this.instanceID,
-          panelNumber: this.panelNumber,
-          onReconnect: this.onReconnect
-        })
-
-        // Use callback to set the srcObject
-        this.onReconnect?.(this)
-      }, delay)
-    })
-
-    info('added subscribe in player reconnectListener', id)
-  }
+  // Add other public methods (pause, unpause, etc.)
 }
+
+export default MedoozePlayer
